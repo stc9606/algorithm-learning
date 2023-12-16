3d3201d8a074570a5e4cbfc39db7f0e961583df8
s_chuli <stc9606@naver.com>
2023-12-16 23:17:09 +0900

    백준(2605) 줄세우기 문제풀이

---
+++ src/com/algorithm/practice/example/덱.java
@@ -0,0 +1,9 @@
+package com.algorithm.practice.example;
+
+public class 덱 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/com/algorithm/practice/example/줄세우기.java
@@ -0,0 +1,25 @@
+package com.algorithm.practice.example;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class 줄세우기 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        int N = sc.nextInt();
+
+        for(int i=0;i<N;i++) {
+            int num = sc.nextInt();
+
+            if(num == 0) list.add(i+1);
+            else list.add(list.size()-num, i+1);
+        }
+
+        for(int i :list)
+            System.out.print(i+" ");
+
+    }
+}
