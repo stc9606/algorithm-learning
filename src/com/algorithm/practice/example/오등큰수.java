fe86cb72e251d8ea2d31e1e28a3e50419ffeb6e5
scnoh <stc9606@naver.com>
2023-12-24 23:53:08 +0900

    백준(1152) 단어의개수 문제풀이

--- src/com/algorithm/practice/example/오등큰수.java
+++ src/com/algorithm/practice/example/오등큰수.java
@@ -1,2 +1,45 @@
-package com.algorithm.practice.example;public class 오등큰수 {
+package com.algorithm.practice.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class 오등큰수 {
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            StringBuilder result = new StringBuilder();
+
+            int N = Integer.parseInt(reader.readLine());
+            int[] sequence = new int[N];
+            int[] count = new int[1_000_001];
+
+            StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+            for (int i = 0; i < N; i++) {
+                sequence[i] = Integer.parseInt(tokenizer.nextToken());
+                count[sequence[i]]++;
+            }
+
+            Stack<Integer> stack = new Stack<>();
+            for (int i = 0; i < N; i++) {
+                while (!stack.isEmpty() && count[sequence[i]] > count[sequence[stack.peek()]]) {
+                    sequence[stack.pop()] = sequence[i];
+                }
+
+                stack.push(i);
+            }
+
+            while (!stack.isEmpty()) {
+                sequence[stack.pop()] = -1;
+            }
+
+            for (int i = 0; i < N; i++) {
+                result.append(sequence[i]).append(" ");
+            }
+
+            System.out.println(result);
+        }
+    }
 }
--- src/com/algorithm/practice/example/오큰수.java
+++ src/com/algorithm/practice/example/오큰수.java
@@ -1,2 +1,43 @@
-package com.algorithm.practice.example;public class 오큰수 {
+package com.algorithm.practice.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class 오큰수 {
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            StringBuilder result = new StringBuilder();
+            Stack<Integer> stack = new Stack<>();
+
+            int N = Integer.parseInt(reader.readLine());
+            int[] sequence = new int[N];
+
+            StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+            for (int i = 0; i < N; i++) {
+                sequence[i] = Integer.parseInt(tokenizer.nextToken());
+            }
+
+            for (int i = 0; i < N; i++) {
+                while (!stack.isEmpty() && sequence[stack.peek()] < sequence[i]) {
+                    sequence[stack.pop()] = sequence[i];
+                }
+                stack.add(i);
+            }
+
+            while (!stack.isEmpty()) {
+                sequence[stack.pop()] = -1;
+            }
+
+            for (int i = 0; i < N; i++) {
+                result.append(sequence[i]).append(" ");
+            }
+
+            System.out.println(result);
+        }
+    }
+
 }
