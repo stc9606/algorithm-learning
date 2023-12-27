bc0a679fc36bb18ce14c299eb6dd798cc9dfde23
s_chuli <stc9606@naver.com>
2023-12-27 19:13:56 +0900

    백준(10799) 쇠막대기 문제풀이

--- src/com/algorithm/practice/example/쇠막대기.java
+++ src/com/algorithm/practice/example/쇠막대기.java
@@ -1,2 +1,38 @@
-package com.algorithm.practice.example;public class 쇠막대기 {
+package com.algorithm.practice.example;
+
+import java.io.*;
+import java.util.Stack;
+
+public class 쇠막대기 {
+
+    public static void main(String[] args) throws IOException {
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))) {
+
+            String[] input = reader.readLine().split("");
+            Stack<String> stack = new Stack<>();
+
+            int total = 0;
+            for (int i = 0; i < input.length; i++) {
+                String s = input[i];
+
+                if ("(".equals(s)) {
+                    stack.push(s);
+                } else {
+                    String prev = input[i-1];
+                    if ("(".equals(prev)) {
+                        stack.pop();
+                        total += stack.size();
+                    } else {
+                        total++;
+                        stack.pop();
+                    }
+                }
+            }
+            writer.write(String.valueOf(total));
+        }
+
+    }
+
 }
